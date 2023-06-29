4007865faa605300a21f73d17a4cef5834c46bb1
alemarostica <alemarostica@virgilio.it>
2023-06-29 14:22:04 +0200

    aggiunto time dump

--- diff.c
+++ diff.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <time.h>
 
 int main(void){
     remove("file2.html");
 
     char * fetch2 = {"./fetch > file2.html"};
-    char * diff = {"diff file1.html file2.html"};
+    char * diff = {"diff -q file1.html file2.html"};
+
+    char * c_time_string;
+    time_t current_time;
 
     while(1){
         system(fetch2);
         sleep(2);
-        system(diff);
+        if(0 != (system(diff))){
+            current_time = time(NULL);
+            c_time_string = ctime(&current_time);
+            fprintf(stdout, "%s\n: ", c_time_string);
+        }
         sleep(8);
     }
 
